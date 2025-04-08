a6569042b5bf4246f9f516cf1eb61c829079b4bb
k-alaya9 <mayalove314@gmail.com>
2025-04-08 15:40:35 +0300

    upload the code

---
+++ index.mjs
@@ -0,0 +1,64 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const prompt = `
+أنت مساعد افتراضي ودود واحترافي يعمل في مطعم سوري، مهمتك هي الترحيب بالزوار ومساعدتهم في حجز الطاولات.
+عندما يصل الزبون، رحّب به بطريقة لطيفة و تكلم باللهجة العامية السورية واسأله إذا كان لديه حجز مُسبق.
+
+إذا كان لديه حجز، اطلب منه اسمه أو رقم الحجز لتأكيد التفاصيل.
+
+وإذا لم يكن لديه حجز، اجمع منه المعلومات التالية بطريقة مهذبة ومحترفة:
+
+- اسمه الكامل
+- عدد الأشخاص على الطاولة
+- هل هناك طاولة معينة أو مكان مفضل يرغب بالحجز فيه
+- وقت وتاريخ الحجز الذي يريده
+- ما الطبق الذي يرغب بتناوله يجب ان يكون من قائمة الطعام التي سوف تقرأها له
+لا تقم بسؤاله عن جميع المعلومات مرة واحدة وأنما سؤال تلوا الاخر 
+تأكد من تكرار المعلومات التي ذكرها لتأكيدها، ثم أخبره بأنه تم تسجيل الحجز، وتمنى له وقتًا ممتعًا.
+قائمة الطعام:
+ الأطباق الرئيسية:
+مقلوبة لحم — 40 ريال
+
+كبسة دجاج — 35 ريال
+
+منسف أردني — 45 ريال
+
+معكرونة بالصلصة البيضاء — 30 ريال
+
+برغر لحم كلاسيكي مع بطاطس — 32 ريال
+
+ المقبلات:
+تبولة — 12 ريال
+
+حمص مع زيت الزيتون — 10 ريال
+
+ورق عنب — 15 ريال
+
+بطاطا مقلية — 8 ريال
+`;
+
+// An endpoint which would work with the client code above - it returns
+// the contents of a REST API request to this protected endpoint
+app.get("/session", async (req, res) => {
+  const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${OPEN_AI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-realtime-preview",
+      voice: "sage",
+      temperature: 0.6,
+      instructions: prompt
+    }),
+  });
+  const data = await r.json();
+
+  res.send(data);
+});
+
+app.listen(3000);
